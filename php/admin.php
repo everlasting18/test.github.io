29cd4e4f5804b8d46a0e2ec556f6ac4205314100
Thai0702 <levanthai20072002@gmail.com>
2023-11-19 11:27:44 +0700

    updatephp

---
+++ php/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima impedit quisquam excepturi magni optio a, ea libero consequuntur dolorem deleniti ipsa odit, officia dolore alias veritatis dicta doloremque! Magnam!
+</body>
+</html>
