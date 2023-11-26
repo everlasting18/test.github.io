1fa030f2b120163112d67567242656ed7c2c23d1
everlasting18 <trantanphat2002@gmail.com>
2023-11-26 11:08:03 +0700

    updatehaha

--- admin/home.php
+++
@@ -1,1 +0,0 @@
-<h1>Control _ Panel</h1>
---
+++ doan/app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+public function Index()
+{
+    return view('userprofile');
+}
+public function Index1()
+{
+    return view('userprofile');
+}
+public function Index2()
+{
+    return view('userprofile');
+}
+public function Index3()
+{
+    return view('userprofile');
+}
+}
